7154a639d9bd87f02e4c01606d15102501dd5366
Chromium7 <christofersaputra29@gmail.com>
2021-02-25 15:33:33 +0700

    Add api and frontend apps

--- controller/settings.py
+++ controller/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
+    'rest_framework',
+    # Local
+    'api.apps.ApiConfig',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
